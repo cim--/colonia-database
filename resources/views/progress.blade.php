24e4859b662b5626008606ad54799316ffb45d5c
cim <cim@oolite.org>
2017-03-26 11:11:35 +0100

    More progress advice

--- resources/views/progress.blade.php
+++ resources/views/progress.blade.php
@@ -23,7 +23,7 @@
 
 <h2>Factions needing pending state updates</h2>
 @if (count($pendingupdate) > 0)
-<p>The following factions do not have pending state updates today. You will need to enter the system to view the pending states in the right panel</p>
+<p>The following factions do not have pending state updates today. You will need to enter the system to view the pending states in the right panel. For a comprehensive survey, it is best to start by doing the Core outposts - as these have many factions only present there - then go to hub systems such as Dubbuennel where many CEI factions are present. It is not absolutely necessary to update every faction every day as pending states usually change slowly.</p>
 <ul class='compact'>
   @foreach ($pendingupdate as $faction)
   <li><a href="{{route('factions.edit',$faction->id)}}">{{$faction->name}}</a></li>
@@ -37,7 +37,7 @@
 
 <h2>Systems needing report updates</h2>
 @if (count($influenceupdate) > 0)
-<p>The following systems do not have report updates today. You will need to dock at a station in the system to view traffic, crime and bounty reports in the local Galnet</p>
+<p>The following systems do not have report updates today. You will need to dock at a station in the system to view traffic, crime and bounty reports in the local Galnet. This does not need daily updates for everywhere!</p>
 <ul class='compact'>
   @foreach ($reportsupdate as $system)
   <li><a href="{{route('systems.editreport',$system->id)}}">{{$system->displayName()}}</a></li>
--- resources/views/systems/editreport.blade.php
+++ resources/views/systems/editreport.blade.php
@@ -8,9 +8,10 @@
 
 <h2>Edit reports</h2>
 {!! Form::open(['route' => ['systems.updatereport', $system->id], 'method'=>'PUT']) !!}
+<p>Reports are available in the local galnet at a station. In low-activity systems, there may be no Crime or Bounties report - in this case, put zero. For crime and bounties, enter the number of credits, not the number of incidents.</p>
 <table class='table table-bordered'>
   <thead>
-	<tr><th>Date</th><th>Traffic Total</th><th>Crime</th><th>Bounties</th></tr>
+	<tr><th>Date</th><th>Traffic Total</th><th>Crime</th><th>Bounties (Cr.)</th></tr>
   </thead>
   <tbody>
 	@if ($latest)
