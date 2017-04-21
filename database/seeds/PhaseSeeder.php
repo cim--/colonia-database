7ccb88cfa940d30725e0fb3572f62ccd59fc11ed
cim <cim@oolite.org>
2017-04-21 09:27:56 +0100

    Add to the phase seeder for easier rebuilds

--- database/seeds/PhaseSeeder.php
+++ database/seeds/PhaseSeeder.php
@@ -11,7 +11,7 @@ class PhaseSeeder extends Seeder
      */
     public function run()
     {
-        $phases = ["Founder", "Core", "CEI 1", "CEI 2", "CEI 3", "CEI 4", "CEI 5"];
+        $phases = ["Founder", "Core 1", "CEI 1", "CEI 2", "CEI 3", "Core 2", "CEI 4", "CEI 5", "CEI 6"];
         foreach ($phases as $idx => $phase) {
             $obj = new App\Models\Phase;
             $obj->name = $phase;
