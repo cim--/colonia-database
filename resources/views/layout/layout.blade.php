ae5201f0b2356e39ce531d16e83c5fc71ccfd780
cim <cim@oolite.org>
2017-06-17 08:05:11 +0100

    Add database backup link

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -33,7 +33,7 @@
 		  To discuss this information, volunteer to help collect it, or suggest site improvements, use the #colonia-bgs channel on <a href="https://eliteccn.com/welcome/join-ccn/">the CCN Discord</a>, or contact Ian Doncaster
 		</p>
 		<p>
-		  The database is open source software under the GNU GPL version 3 - <a href='https://github.com/cim--/colonia-database'>source available on Github</a>
+		  The database is open source software under the GNU GPL version 3 - <a href='https://github.com/cim--/colonia-database'>source available on Github</a> - <a href='/cdb.sql'>daily database backup</a>
 		</p>
       </footer>
 	</body>
