55896c488eb5be3d9abb6cc84145a3d9764cf176
cim <cim@oolite.org>
2017-12-18 07:26:57 +0000

    Only Alert if not already known

--- app/Models/Alert.php
+++ app/Models/Alert.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Alert extends Model
 {
     public static function alert($contents) {
+        if (Alert::where('alert', $contents)->where('processed', false)->count() > 0) {
+            return; // already alerted
+        }
         $alert = new Alert;
         $alert->alert = $contents;
         $alert->save();
