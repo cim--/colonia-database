9398679c3290383d59be6b078565f10b39c739c6
cim <cim@oolite.org>
2018-01-06 10:17:28 +0000

    Add link to forum thread

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -29,7 +29,7 @@
 	  </div>
       <footer>
 		<p>
-		  To discuss this information, volunteer to help collect it, or suggest site improvements, join the <a href="https://discord.gg/fE49mGw">Colonia Census Discord</a>, or contact Ian Doncaster
+		  To discuss this information, volunteer to help collect it, or suggest site improvements, join the <a href="https://discord.gg/fE49mGw">Colonia Census Discord</a>, <a href="https://forums.frontier.co.uk/showthread.php/398547-The-Colonia-Census-history-and-geography-resource-for-the-Colonia-region">post in the forum thread</a>, or contact Ian Doncaster
 		</p>
 		<p>
 		  The database is open source software under the GNU GPL version 3 - <a href='https://github.com/cim--/colonia-database'>source available on Github</a> - <a href='/cdb.sql'>daily database backup</a>
