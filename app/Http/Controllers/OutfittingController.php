09eaf913f7f616ad68b80d22ec0b32813b03879e
cim <cim@oolite.org>
2018-01-14 09:13:58 +0000

    Update availability to exclude unpurchasable modules
    (requires large ship but only stations have no large pads)

--- app/Http/Controllers/OutfittingController.php
+++ app/Http/Controllers/OutfittingController.php
@@ -12,19 +12,20 @@ class OutfittingController extends Controller
     
     public function index() {
         $coremodules = Moduletype::where('type', 'core')->whereHas('modules.stations')->with(['modules' => function($q) {
-            $q->whereHas('stations');
+                $q->isAvailable();
         }])->orderBy('description')->get();
 
         $optionalmodules = Moduletype::where('type', 'optional')->whereHas('modules.stations')->with(['modules' => function($q) {
-            $q->whereHas('stations');
+                $q->isAvailable();
         }])->orderBy('description')->get();
 
         $optionalnsmodules = Moduletype::where('type', 'optionalns')->with(['modules' => function($q) {
+                // all non-large so isAvailable not needed
             $q->withCount('stations');
         }])->orderBy('description')->get();
         
         $armours = Moduletype::where('type', 'armour')->with(['modules' => function($q) {
-            $q->whereHas('stations');
+                $q->isAvailable();
         }])->orderBy('description')->get();
         $ships = Module::whereHas('moduletype', function($q) {
             $q->where('type', 'armour');
@@ -36,18 +37,21 @@ class OutfittingController extends Controller
         ksort($shiptypes);
 
         $weapons = Moduletype::where('type', 'hardpoint')->with(['modules' => function($q) {
+                // all non-large so isAvailable not needed
                 $q->withCount('stations');
             }])->orderBy('description')->get();
 
         $utilities = Moduletype::where('type', 'utility')->whereHas('modules', function($q) {
             $q->whereIn('type', ['A','B','C','D','E']);
         })->with(['modules' => function($q) {
+                // all non-large so isAvailable not needed
                 $q->withCount('stations');
             }])->orderBy('description')->get();
         
         $utilitiesns = Moduletype::where('type', 'utility')->whereHas('modules', function($q) {
             $q->whereNotIn('type', ['A','B','C','D','E']);
         })->with(['modules' => function($q) {
+                // all non-large so isAvailable not needed
                 $q->withCount('stations');
             }])->orderBy('description')->get();
 
--- app/Models/Module.php
+++ app/Models/Module.php
@@ -15,6 +15,23 @@ class Module extends Model
     {
         return $this->belongsToMany('App\Models\Station');
     }
+
+    /* A module is available if either:
+     * - it doesn't require a large ship and is available at a station
+     * - it is available at a station with large pads
+     */
+    public function scopeIsAvailable($q) {
+        return $q->where(function($smq) {
+            $smq->whereHas('stations')
+                ->where('largeship', 0);
+        })->orWhere(function($lq) {
+            $lq->whereHas('stations', function($ssq) {
+                $ssq->whereHas('stationclass', function ($scq) {
+                    $scq->where('hasLarge', 1);
+                });
+            });
+        });
+    }
     
     public function displayName()
     {
