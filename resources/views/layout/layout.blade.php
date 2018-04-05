01cafab0581cb822e927c6904265c2ea32e3050b
cim <cim@oolite.org>
2018-04-05 07:36:59 +0100

    Update footer text

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -39,7 +39,7 @@
 		  The database is open source software under the GNU GPL version 3 - <a href='https://github.com/cim--/colonia-database'>source available on Github</a> - <a href='/cdb.sql.gz'>daily database backup</a>
 		</p>
 		<p>
-		  The site is created using data from Elite: Dangerous, with the permission of Frontier Developments plc, for non-commercial purposes. It is not endorsed by nor reflects the views or opinions of Frontier Developments.
+		  The site is created using data from <a href='https://www.elitedangerous.com/'>Elite: Dangerous</a>, with the permission of Frontier Developments plc, for non-commercial purposes. It is not endorsed by nor reflects the views or opinions of Frontier Developments and no employee of Frontier Developments was involved in the making of it.
 		</p>
 		<p>
 		  The site uses cookies for essential purposes only. No personal data is stored in the cookies unless you use the Login system. The majority of site features do not require a login, and no personal data will be shared with any third party.
