9acd737c97b22f8fc7d18db4dea1ce8b493a28ed
cim <cim@oolite.org>
2018-11-03 12:44:14 +0000

    Fix display of weapons/utilities to distinguish missing from nonexistent

--- resources/views/outfitting/utilitycell.blade.php
+++ resources/views/outfitting/utilitycell.blade.php
@@ -1,8 +1,8 @@
 <td>
-  @if ($utility->modules->where('type', $class)->count() == 0)
+  @if ($utility->modules()->where('type', $class)->count() == 0)
   -
   @else
-  @if ($utility->modules->where('type', $class)->first()->stations_count > 0)
+  @if ($utility->modules->where('type', $class)->first())
   @include('layout.yes')
   @else
   @include('layout.no')
--- resources/views/outfitting/weaponcell.blade.php
+++ resources/views/outfitting/weaponcell.blade.php
@@ -1,8 +1,8 @@
 <td>
-  @if ($weapon->modules->where('size', $size)->where('type', $mount)->count() == 0)
+  @if ($weapon->modules()->where('size', $size)->where('type', $mount)->count() == 0)
   -
   @else
-  @if ($weapon->modules->where('size', $size)->where('type', $mount)->first()->stations_count > 0)
+  @if ($weapon->modules->where('size', $size)->where('type', $mount)->first())
   @include('layout.yes')
   @else
   @include('layout.no')
