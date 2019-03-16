3cdc9c3e52ad43aff3c9568a3f49e90ed25c6597
Chris Morris <cim@oolite.org>
2019-03-16 09:18:05 +0000

    Adjust blueprint display to be clearer

--- resources/views/components/blueprint.blade.php
+++ resources/views/components/blueprint.blade.php
@@ -1,22 +1,22 @@
 @if ($blueprint->level < 2)
 <span class='blueprint blueprint-level1' title='Grade 1 blueprints available, researching Grade 2'>
-  <strong>&#x2699;</strong><span>&#x2699;</span><span>&#x2699;</span><span>&#x2699;</span><span>&#x2699;</span>
+  <strong>&#x2699;&#xFE0E;</strong><span>&#x26EE;&#xFE0E;</span><span>&#x26EE;&#xFE0E;</span><span>&#x26EE;&#xFE0E;</span><span>&#x26EE;&#xFE0E;</span>
 
 @elseif ($blueprint->level < 3)
 <span class='blueprint blueprint-level2' title='Grade 2 blueprints available, researching Grade 3'>
-  <strong>&#x2699;</strong><strong>&#x2699;</strong><span>&#x2699;</span><span>&#x2699;</span><span>&#x2699;</span>
+  <strong>&#x2699;&#xFE0E;</strong><strong>&#x2699;&#xFE0E;</strong><span>&#x26EE;&#xFE0E;</span><span>&#x26EE;&#xFE0E;</span><span>&#x26EE;&#xFE0E;</span>
 
 @elseif ($blueprint->level < 4)
 <span class='blueprint blueprint-level3' title='Grade 3 blueprints available, researching Grade 4'>
-  <strong>&#x2699;</strong><strong>&#x2699;</strong><strong>&#x2699;</strong><span>&#x2699;</span><span>&#x2699;</span>
+  <strong>&#x2699;&#xFE0E;</strong><strong>&#x2699;&#xFE0E;</strong><strong>&#x2699;&#xFE0E;</strong><span>&#x26EE;&#xFE0E;</span><span>&#x26EE;&#xFE0E;</span>
 
 @elseif ($blueprint->level < 5)
 <span class='blueprint blueprint-level4' title='Grade 4 blueprints available, researching Grade 5'>
-  <strong>&#x2699;</strong><strong>&#x2699;</strong><strong>&#x2699;</strong><strong>&#x2699;</strong><span>&#x2699;</span>
+  <strong>&#x2699;&#xFE0E;</strong><strong>&#x2699;&#xFE0E;</strong><strong>&#x2699;&#xFE0E;</strong><strong>&#x2699;&#xFE0E;</strong><span>&#x26EE;&#xFE0E;</span>
 
 @elseif ($blueprint->level == 5)
 <span class='blueprint blueprint-level5' title='Grade 5 blueprints available'>
-  <strong>&#x2699;</strong><strong>&#x2699;</strong><strong>&#x2699;</strong><strong>&#x2699;</strong><strong>&#x2699;</strong>
+  <strong>&#x2699;&#xFE0E;</strong><strong>&#x2699;&#xFE0E;</strong><strong>&#x2699;&#xFE0E;</strong><strong>&#x2699;&#xFE0E;</strong><strong>&#x2699;&#xFE0E;</strong>
 
 @endif
   @if (isset($fractional) && $fractional)
