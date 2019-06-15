27f7ce4ec5600e037687df3f04a91942d2c0fe03
Chris Morris <cim@oolite.org>
2019-06-15 08:43:03 +0100

    New economy icons

---
+++ resources/views/icons/economies/hightechindext.blade.php
@@ -0,0 +1,1 @@
+<span title='High-Tech-Industrial-Extraction' class='icon-economy-hightech'>&#x269B;&#xFE0E;<sub><span class='icon-economy-industrial'>&#x2699;&#xFE0E;</span><span class='icon-economy-extraction'>&#x2692;&#xFE0E;</span></sub></span>
---
+++ resources/views/icons/economies/hightechtourism.blade.php
@@ -0,0 +1,1 @@
+<span title='High-Tech / Tourism' class='icon-economy-hightech'>&#x269B;&#xFE0E;<sub><span class='icon-economy-tourism'>&#x26F1;&#xFE0E;</span></sub></span>
