29ea64c92a435520b7ee537e76db6b0ee4b4f8a1
Chris Morris <cim@oolite.org>
2019-08-10 09:14:21 +0100

    Hide navbar links

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -3,10 +3,10 @@
 	<li><div>Introduction</div>
 	  <ul >
 		<li><a href="{{route('index')}}">Home</a></li>
-		<li><a title='About the Census' href="{{route('intro.about')}}">Census</a></li>
+{{--		<li><a title='About the Census' href="{{route('intro.about')}}">Census</a></li>
 		<li><a title='The story of Colonia' href="{{route('intro.story')}}">Story</a></li>
 		<li><a title='Differences between Colonia and Sol' href="{{route('intro.new')}}">Differences</a></li>
-		<li><a title='Comparing Colonia to other settlements' href="{{route('intro.regions')}}">Regions</a></li>
+  <li><a title='Comparing Colonia to other settlements' href="{{route('intro.regions')}}">Regions</a></li> --}}
      	<li><a href="{{route('intro.icons')}}">Icon Key</a></li>
 	  </ul>
 	</li>
@@ -14,8 +14,8 @@
 	  <ul>
 		<li><a href="{{route('map')}}">Map</a></li>
 		<li><a href="{{route('distances')}}">Distances</a></li>
-		<li><a href="{{route('history')}}">History</a></li>
-     	<li><a href="{{route('history.trends')}}">Activity</a></li>
+        {{--		<li><a href="{{route('history')}}">History</a></li>
+          <li><a href="{{route('history.trends')}}">Activity</a></li> --}}
 	  </ul>
 	</li>
 	<li><div>Catalogues</div>
@@ -24,8 +24,8 @@
 		<li><a href="{{route('factions.index')}}">Factions</a></li>
 		<li><a href="{{route('stations.index')}}">Stations</a></li>
      	<li><a href="{{route('megaships.index')}}">Megaships</a></li>
-        <li><a href="{{route('installations.index')}}">Installations</a></li>
-        <li><a href="{{route('sites.index')}}">Sites</a></li>
+      {{--        <li><a href="{{route('installations.index')}}">Installations</a></li>
+        <li><a href="{{route('sites.index')}}">Sites</a></li> --}}
 	  </ul>
 	</li>
 	<li><div>Reports</div>
@@ -41,11 +41,11 @@
 	<li><div>Economy and Technology</div>
 	  <ul class='compactnav'>
 		<li><a href="{{route('reserves')}}">Reserves</a></li>
-     	<li><a href="{{route('effects')}}">Effects</a></li>
-        <li><a href="{{route('specialisation')}}">Specialities</a></li>
+      {{--     	<li><a href="{{route('effects')}}">Effects</a></li>
+         <li><a href="{{route('specialisation')}}">Specialities</a></li> 
 		<li><a href="{{route('trade')}}">Trading</a></li>
 		<li><a href="{{route('logistics')}}">Logistics</a></li>
-		<li><a href="{{route('missions.index')}}">Missions</a></li>
+         <li><a href="{{route('missions.index')}}">Missions</a></li> --}}
      	<li><a href="{{route('outfitting')}}">Outfitting</a></li>
 		<li><a href="{{route('outfitting.shipyard')}}">Shipyard</a></li>
 		<li><a href="{{route('engineers.index')}}">Engineers</a></li>
