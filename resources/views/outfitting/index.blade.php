20c09d5d3126a28289568aa58d080072adb60a67
Chris Morris <cim@oolite.org>
2019-10-19 09:02:59 +0100

    Local text update

--- resources/views/outfitting/index.blade.php
+++ resources/views/outfitting/index.blade.php
@@ -6,7 +6,7 @@
 
 <p>The best outfitting is available from the stations which have a <a href="{{route('stations.index')}}#%22high-quality%22">"high quality outfitting"</a> facility. These tables summarise the module production for the Witch Head region as a whole as well as the <a href='{{route('engineers.index')}}'>engineering blueprints</a> available locally.</p>
 
-<p>Tech Broker modules are available from Bolden's Enterprise in Tir (Human) and Jaques Station in Colonia (Guardian), depending on previous unlocks. Some of the Human ones can be unlocked with resources available locally.</p>
+<p>Tech Broker modules are available from Bray Landing in Witch Head Sector LC-V c2-9.</p>
 
 @if ($reqcurrent)
 <p>Note: other modules may be possible to produce but not currently in stock. See the individual module pages for details of stock levels, or <a href="{{route('outfitting')}}">view a summary of production capability</a>.</p>
