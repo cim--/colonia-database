12c42f76843a57b4e1009f8170d4d2af85452925
Chris Morris <cim@oolite.org>
2020-03-11 20:33:57 +0000

    Minor navbar tweak

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,12 +1,11 @@
 <nav>
   <ul>
 	<li><div>Introduction</div>
-	  <ul class='compactnav'>
+	  <ul>
 		<li><a href="{{route('index')}}">Home</a></li>
 		<li><a title='About the Census' href="{{route('intro.about')}}">Census</a></li>
 		<li><a title='The story of Colonia' href="{{route('intro.story')}}">Story</a></li>
 		<li><a title='Differences between Colonia and Sol' href="{{route('intro.new')}}">Differences</a></li>
-		<li><a title='Comparing Colonia to other settlements' href="{{route('intro.regions')}}">Regions</a></li>
                 <li><a title='Colonia Radio' href='{{route('radio')}}'>Radio</a></li>
      	<li><a title='Key for icons' href="{{route('intro.icons')}}">Icons</a></li>
 	  </ul>
@@ -17,6 +16,7 @@
 		<li><a href="{{route('distances')}}">Distances</a></li>
 		<li><a href="{{route('history')}}">History</a></li>
      	<li><a href="{{route('history.trends')}}">Activity</a></li>
+		<li><a title='Comparing Colonia to other settlements' href="{{route('intro.regions')}}">Regions</a></li>
 	  </ul>
 	</li>
 	<li><div>Catalogues</div>
