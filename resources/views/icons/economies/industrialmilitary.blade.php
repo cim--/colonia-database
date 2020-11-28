cb7451d9153696f3e84782046f21871529353f7f
Chris Morris <cim@oolite.org>
2020-11-28 13:37:10 +0000

    New hybrid economies

---
+++ resources/views/icons/economies/industrialmilitary.blade.php
@@ -0,0 +1,1 @@
+<span title='Industrial-Military' class='icon-economy-industrial'>&#x2699;<sub><span title='Military' class='icon-economy-military'>&#x2694;&#xFE0E;</span></sub></span>
---
+++ resources/views/icons/economies/refineryextraction.blade.php
@@ -0,0 +1,1 @@
+<span title='Refinery-Extraction' class='icon-economy-refinery'>&#x2697;&#xFE0E;<sub><span class='icon-economy-extraction'>&#x2692;&#xFE0E;</span></sub></span>
