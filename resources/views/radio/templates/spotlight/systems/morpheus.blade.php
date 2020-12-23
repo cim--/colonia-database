d05a30ca30a95c37a9b555c73ba92284f6078743
Chris Morris <cim@oolite.org>
2020-12-23 10:13:25 +0000

    Update radio for new version of Colonia Guide

--- resources/views/radio/templates/spotlight/systems/morpheus.blade.php
+++ resources/views/radio/templates/spotlight/systems/morpheus.blade.php
@@ -1,2 +1,3 @@
 <p>This refinery system was established by the Order of the West's as part of the Colonia Expansion Initiative, and is particularly efficient at production of Beryllium, Synthetic Fabrics and Explosives.</p>
 <p>Like many systems near the nebula, Morpheus is inhabited by Luteolum umbrella molluscs, with the largest colony being in a lagrange cloud near the 1a moon.</p>
+<p>In late October 3306 the system became embroiled in the nearby Third Regional Conflict, and was requisitioned by Societas Eruditorum de Civitas Dei forces to help reinforce supply lines against Nameless assaults. The opinions of the enigmatic Order on these events remain uncertain.</p>
