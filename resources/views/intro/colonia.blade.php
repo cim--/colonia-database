4fc0def6c6bff838eaa59a8b355df730323535f4
Chris Morris <cim@oolite.org>
2020-12-23 10:17:36 +0000

    Extra link

--- resources/views/intro/colonia.blade.php
+++ resources/views/intro/colonia.blade.php
@@ -11,6 +11,7 @@
   <li><a href="{{route('intro.new')}}">Differences between Colonia and Sol</a> - useful information for new visitors on what to expect out here</li>
   <li><a href='{{route('radio')}}'>Colonia Radio</a> - live information on regional conditions</li>
   <li><a href='https://drive.google.com/file/d/1aXPVsYBYjhMLPp4zu9RJfKKoxZFgP9Iz/view?usp=sharing'>A Tourist Guide to Colonia</a> (PDF) - detailed information on all of Colonia's systems, and their role in the region as a whole.</li>
+  <li><a href='https://drive.google.com/file/d/1HOSLLTjNMdVJqTdCjh4MVFIP8eIk8XEG/view?usp=sharing'>A history of the Third Regional Conflict</a> (PDF) - a guide to Colonia's largest political dispute.</li>
 </ul>
     
 @endsection
