99573cc5ca3e3192749dd46f2708abb2d6d4a59c
Chris Morris <cim@oolite.org>
2021-10-31 21:59:31 +0000

    Another history update

--- resources/views/intro/new/mining.blade.php
+++ resources/views/intro/new/mining.blade.php
@@ -2,4 +2,4 @@
 	  
 <p>The briefness of human occupation of the region means that all mineral reserves are classified as Pristine in quality. The majority of inhabited systems have some mining opportunities, though relatively few have metallic asteroids. Mining options are shown for inhabited systems in the system catalogue, though experienced miners may find better opportunities in nearby uninhabited systems.</p>
 
-<p>A major Painite hotspot has been found in the Randgnid 4 A-ring, and a strong local economy is generally favourable to the selling of gemstones mined from asteroid cores, though prices can vary rapidly over a few days with local demands.</p>
+<p>A strong local economy is generally favourable to the selling of gemstones mined from asteroid cores, though prices can vary rapidly over a few days with local demands, and the best prices may require some patience.</p>
